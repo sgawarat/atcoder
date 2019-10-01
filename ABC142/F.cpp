d7dabb8efba10bb161cc5553b0b682b776eb3fcd
sgawarat <sgawarat@gmail.com>
2019-10-01 14:41:31 +0900

    [ABC142] 少しWAするようFの解答を修正する

--- ABC142/F.cpp
+++ ABC142/F.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <unordered_map>
 #include <unordered_set>
@@ -5,6 +6,8 @@
 
 // 入次数と出次数がともに1である ⇒ 辺の向きが揃ったループ（v1 -> v2 -> ... -> vn -> v1）
 // ⇒ ある点を出発して戻って来られるかを調べる
+// 
+// ループを構成する経路が複数あれば、より短い方が条件を満たす（v1 -> v2 -> v3, v1 -> v3の場合、条件に合わない(v1,v2,v3)を採用しない）
 int main_F() {
   int N;  // 頂点の数
   int M;  // 辺の数
@@ -42,6 +45,9 @@ int main_F() {
       } else if (!finished[child]) {
         // 2回目の訪問ならば、そこにループがある
         quit = true;
+
+        // 経路を見つけたループのみに切り詰める
+        path.erase(path.begin(), std::find(path.begin(), path.end(), child));
         break;
       }
     }
@@ -64,21 +70,39 @@ int main_F() {
     // ループが見つからなかった場合
     std::cout << "-1";
   } else {
-    // ループを含む経路を見つけた場合
+    // ループを見つけた場合
 
-    // 見つけたループの中で最も短いものを選ぶ
-    const auto& children = children_map[path.back()];
-    std::unordered_set<int> child_set(children.begin(), children.end());
-    auto iter = --path.end();
-    while (iter != path.begin()) {
-      const int v = *--iter;
-      if (child_set.find(v) != child_set.end()) break;
+    // ループにショートカットがあればそちらを採用する
+    std::vector<bool> unused(path.size());  // 実際には使われない頂点かどうかのフラグ列
+    for (size_t i = 0; i < path.size(); ++i) {
+      if (!unused[i]) {
+        // 探しやすいようにsetを作る
+        const auto& children = children_map[path[i]];
+        std::unordered_set<int> child_set(children.begin(), children.end());
+
+        // 経路上で最も離れた頂点との辺を探す（[i+1,i+#paths)を逆順に）
+        for (size_t j = path.size() - 1 + i; j > i; --j) {
+          if (child_set.find(path[j % path.size()]) != child_set.end()) {
+            // その2頂点でループを短絡する（[i+1, j-1]を取り除く）
+            for (size_t k = j - 1; k > i; --k) {
+              unused[k % path.size()] = true;
+            }
+            break;
+          }
+        }
+      }
+    }
+
+    // 実際に使われる頂点の個数を数える
+    size_t count = 0;
+    for (auto b : unused) {
+      if (!b) count++;
     }
 
     // ループの内の頂点を表示する
-    std::cout << (path.end() - iter) << '\n';
-    for (; iter != path.end(); ++iter) {
-      std::cout << *iter << '\n';
+    std::cout << count << '\n';
+    for (size_t i = 0; i < path.size(); ++i) {
+      if (!unused[i]) std::cout << path[i] << '\n';
     }
   }
 
