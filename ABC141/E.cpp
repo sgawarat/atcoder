e7e78f624d5154a00e7ac59bbb63a195b2b30063
sgawarat <sgawarat@gmail.com>
2019-10-04 21:39:24 +0900

    [ABC141] Eをより説明的な記述に修正する

--- ABC141/E.cpp
+++ ABC141/E.cpp
@@ -5,43 +5,42 @@
 #include <vector>
 
 int solve_E() {
-  size_t N;
-  std::string S;
+  size_t N;  // 文字列の長さ
+  std::string S;  // 文字列
   std::cin >> N >> S;
 
-  // 共通文字列の最大長を求める
+  // 一致する部分文字列の最大長を求める
   size_t result = 0;  // 一致する部分文字列の最大長
-  std::vector<size_t> z;  // [0, z[i])と[i, i+z[i])が一致している
+  std::vector<size_t> z;  // [0, z[i])と[i, i+z[i])が一致するときの長さ
   for (size_t l1 = 0; l1 < N - 1; ++l1) {
     // バッファをクリアする
     z.clear();
     z.resize(N - l1, 0);
 
     // Z-Algorithm
-    size_t c = 0;  // [l1, l1+len)と[c, z[c])が一致している
+    size_t beg = 0, end = 0;  // [l1, l1+(end-beg))と一致する[beg, end)
     for (size_t i = 1; i < z.size(); ++i) {
-      if (i + z[i - c] < c + z[c]) {
+      if (i + z[i - beg] < end) {
         // 一致を確認した範囲の中であれば、以前に計算した値を再利用できる
-        z[i] = z[i - c];
+        z[i] = z[i - beg];
       } else {
         // 一致を確認した範囲を外れる場合、改めて一致する部分を探す
         const size_t l2 = l1 + i;
-        size_t len = 0;
-        if (c + z[c] > i) len = c + z[c] - i;
+        size_t len = i < end ? (end - i) : 0;  // 一致することが分かっている部分を長さに含める
         for (; l2 + len < N; ++len) {
           if (S[l1 + len] != S[l2 + len]) break;
         }
         z[i] = len;
-        c = i;
+        beg = i;
+        end = i + len;
       }
-    }
-    z[0] = z.size();
 
-    for (size_t i = 0; i < z.size(); ++i) {
+      // 文字列同士が重ならないような長さ（l2-l1=i）を考慮しつつ最大長を求める
       result = std::max(result, std::min(i, z[i]));
     }
   }
 
+  // 結果発表
   std::cout << result;
   return 0;
 }
