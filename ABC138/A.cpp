cf5f41a0a2b929a2dc316f20c4dedf038a0dd4a9
sgawarat <sgawarat@gmail.com>
2019-10-18 21:37:22 +0900

    [ABC138] ACするAを追加する

---
+++ ABC138/A.cpp
@@ -0,0 +1,22 @@
+﻿// 解説前
+#include <iostream>
+#include <string>
+
+int solve_A() {
+  int a;
+  std::cin >> a;
+
+  if (a >= 3200) {
+    std::string s;
+    std::cin >> s;
+    std::cout << s;
+  } else {
+    std::cout << "red";
+  }
+
+  return 0;
+}
+
+int main() {
+  return solve_A();
+}
