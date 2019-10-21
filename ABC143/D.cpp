16ed3bf77685943d3bd74c12f28002f4f32dde1b
sgawarat <sgawarat@gmail.com>
2019-10-21 23:37:35 +0900

    [ABC143] ACするDを追加する

--- ABC143/D.cpp
+++ ABC143/D.cpp
@@ -1,22 +1,32 @@
-﻿// 解説前
+﻿// 解説後
 #include <algorithm>
 #include <iostream>
-#include <map>
 #include <set>
 #include <vector>
 
+// ・互いに区別できるN本の棒から三角形を作る。
+// ・2つの三角形を比べたとき、使われる棒が1つでも一方のみに存在する場合、それらの三角形は異なる。
+// ・このときに取り得る三角形の個数を計算したい。
+// 
+// 三角形を作る3つの棒の長さをa,b,c（a <= b <= c）とする。
+// cは与えられた条件からa-b < b-a < c < a+bである。
+// このとき、b-a <= bなので、b <= c < a+bとなる
+// => 長さが[b, a+b)の範囲にある棒の個数を求めれば良い
+// 
+// 棒の長さのリストをソートすれば、二分探索を使って境界値へのポインタを得られる。
+// => 全体としてO(N^2 * logN)で計算できる
 int solve_D() {
   size_t N;
   std::cin >> N;
 
-  std::vector<uint32_t> lines(N);
-  for (auto& line : lines) {
-    std::cin >> line;
+  std::vector<uint32_t> L(N);
+  for (auto& L_i : L) {
+    std::cin >> L_i;
   }
 
   // 1. 全探索
   //    O(N^3)くらい => TLE
-  auto solver1 = [&]() -> size_t {
+  auto solver1 = [&]() -> uint64_t {
     struct Triangle {
       Triangle(uint32_t x, uint32_t y, uint32_t z) noexcept {
         if (x > y) std::swap(x, y);
@@ -39,12 +49,12 @@ int solve_D() {
     };
 
     std::set<Triangle> set;
-    for (size_t i = 0; i < lines.size() - 2; ++i) {
-      const auto a = lines[i];
-      for (size_t j = i + 1; j < lines.size() - 1; ++j) {
-        const auto b = lines[j];
-        for (size_t k = j + 1; k < lines.size(); ++k) {
-          const auto c = lines[k];
+    for (size_t i = 0; i < L.size() - 2; ++i) {
+      const auto a = L[i];
+      for (size_t j = i + 1; j < L.size() - 1; ++j) {
+        const auto b = L[j];
+        for (size_t k = j + 1; k < L.size(); ++k) {
+          const auto c = L[k];
           if (a < b + c &&
               b < c + a &&
               c < a + b) {
@@ -57,57 +67,47 @@ int solve_D() {
   };
 
   // 2. 二辺の長さから取り得る三角形の個数を数える方法
-  auto solver2 = [&]() -> size_t {
+  auto solver2 = [&]() -> uint64_t {
     constexpr uint32_t MAX_LENGTH = 1000;
 
+    // ソート
+    std::sort(L.begin(), L.end());
+
     // 長さをインデックスとした棒の個数のリストを作る
-    std::vector<uint32_t> edge_counts(1001);
-    for (const auto line : lines) {
-      edge_counts[line]++;
+    std::vector<uint32_t> counts(1001);
+    for (const auto L_i : L) {
+      counts[L_i]++;
     }
 
-    // 長さをインデックスとした三角形の個数の累積和のリストを作る
-    std::vector<uint32_t> triangle_counts(1001);
-    for (size_t i = 1; i < triangle_counts.size(); ++i) {
-      triangle_counts[i] = triangle_counts[i - 1] + (edge_counts[i] > 0 ? 1 : 0);
+    // 長さをインデックスとした棒の個数の累積和のリストを作る
+    std::vector<uint32_t> prefix_sums(counts.size());
+    prefix_sums[0] = counts[0];
+    for (size_t i = 1; i < counts.size(); ++i) {
+      prefix_sums[i] = prefix_sums[i - 1] + counts[i];
     }
 
     // 取り得るすべての三角形の個数を計算する
-    // ここでは、同じ三角形を重複して数えないように各辺の長さをa <= b <= cとする
-    size_t result = 0;
-    for (uint32_t a = 1; a <= MAX_LENGTH; ++a) {
-      const auto a_count = edge_counts[a];
+    uint64_t result = 0;
+    std::vector<uint32_t> used_a(counts.size());
+    std::vector<uint32_t> used_b(counts.size());
+    for (size_t i = 0; i < N - 1; ++i) {
+      const auto a = L[i];
 
-      // 長さaの棒が1つもなければ、三角形は1つも作れない
-      if (a_count == 0) continue;
+      // 使用済みの棒を数える
+      used_a[a]++;
 
-      // a == bのとき、長さaの棒が2本以上ある場合
-      if (a_count >= 2) {
-        // 0 < c < 2aの間に取り得る三角形の個数を計上する
-        // a <= cなので、0 < c < 2a => a <= c <= (2a-1)
-        const auto lhs = a;
-        const auto rhs = std::min(a + a - 1, MAX_LENGTH);
-        result += triangle_counts[rhs] - triangle_counts[lhs - 1];
+      used_b = used_a;
+      for (size_t j = i + 1; j < N; ++j) {
+        const auto b = L[j];
 
-        // cが長さaの棒を使えない場合、その三角形を差し引く
-        if (a_count == 2) result--;
-      }
+        // 使用済みの棒を数える
+        used_b[b]++;
 
-      // a < bのときに取り得る三角形の個数を計算する
-      for (uint32_t b = a + 1; b <= MAX_LENGTH; ++b) {
-        const auto b_count = edge_counts[b];
+        //　b <= c <= (a+b-1)となる三角形の個数を計上する
+        result += prefix_sums[std::min(a + b - 1, MAX_LENGTH)] - prefix_sums[b - 1];
 
-        // 長さbの棒が1つもなければ、三角形は1つも作れない
-        if (b_count == 0) continue;
-
-        //　b-a < c < a+bの間に取り得る三角形の個数を計上する
-        // b-a < bかつb <= cなので、b-a < c < a+b => b <= c <= (a+b-1)
-        const auto lhs = b;
-        const auto rhs = std::min(a + b - 1, MAX_LENGTH);
-        result += triangle_counts[rhs] - triangle_counts[lhs - 1];
-
-        // cが長さbの棒を使えない場合、その三角形を差し引く
-        if (b_count == 1) result--;
+        // 組み合わせの重複を避けるため、使用済みの長さbの棒をを差し引く
+        result -= used_b[b];
       }
     }
 
