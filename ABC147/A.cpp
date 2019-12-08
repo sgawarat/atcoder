27c76f994ab666b9f79d8e578c1b9f27383bd044
sgawarat <sgawarat@gmail.com>
2019-12-08 23:14:59 +0900

    [ABC147] 本戦の結果を追加する

---
+++ ABC147/A.cpp
@@ -0,0 +1,15 @@
+﻿// 本戦
+#include <iostream>
+
+int main() {
+  int A1, A2, A3;
+  std::cin >> A1 >> A2 >> A3;
+
+  if (A1 + A2 + A3 >= 22) {
+    std::cout << "bust";
+  } else {
+    std::cout << "win";
+  }
+
+  return 0;
+}
---
+++ ABC147/B.cpp
@@ -0,0 +1,21 @@
+﻿// 本戦
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string S;
+  std::cin >> S;
+
+  size_t i = 0;
+  size_t j = S.size() - 1;
+  size_t result = 0;
+  while (i < j) {
+    if (S[i] != S[j]) result++;
+    i++;
+    j--;
+  }
+
+  std::cout << result;
+
+  return 0;
+}
